0029cd40720d0c636a70bc97b3158a1fa703c368
semenchenkovital <semenchenkovital@gmail.com>
2016-10-24 14:18:59 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import json
-
+import codecs
 
 def load_data(filepath):
-    pass
-
+    infile=codecs.open(filepath,'r', 'utf-8-sig').read()
+    jsondata = json.loads(infile)
+    return jsondata
 
 def pretty_print_json(data):
-    pass
-
-
+    len_data = len(data)
+    i = 0
+    while i<len_data:
+       print(json.dumps(data[i], ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ': ')))
+       i = i+1
 if __name__ == '__main__':
-    pass
+     file = input("Введите имя файла:")
+     try:
+       data = load_data(file)
+     except:
+       print('Некорректное имя файла или путь к файлу')
+     else:
+         pretty_print_json(data)
